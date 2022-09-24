1dbb9723e538e4859f3b18b73600634bae377897
Alex Skorulis <skorulis@gmail.com>
2022-09-24 12:05:12 +1000

    Add some basic styling

--- src/scene/ProjectCell.tsx
+++ src/scene/ProjectCell.tsx
@@ -1,5 +1,6 @@
 import { Component } from "react"; 
 import { Project } from "../model/Project";
+import CSS from 'csstype';
 
 type ProjectCellProps = {
     project: Project
@@ -12,11 +13,21 @@ export class ProjectCell extends Component<ProjectCellProps, {}> {
     }
 
     render() {
-        return <p>{this.numberString()} {this.props.project.name}</p>
+        return <div style={h1Styles}>
+            <h3>{this.numberString()} {this.props.project.name}</h3>
+            <p>{this.props.project.brief}</p>
+        </div>
+        
     }
 
     numberString() {
         return `#${this.props.project.id}`
     }
 
+}
+
+const h1Styles: CSS.Properties = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "start"
 }
